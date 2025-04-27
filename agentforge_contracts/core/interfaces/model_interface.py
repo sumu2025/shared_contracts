530108cf75c92254373b762b658c19a4e85b4f3e
sumu2025 <weipeacock@gmail.com>
2025-04-28 06:20:25 +0800

    格式化model_interface.py文件以符合Black代码风格规范

--- agentforge_contracts/core/interfaces/model_interface.py
+++ agentforge_contracts/core/interfaces/model_interface.py
@@ -8,7 +8,11 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional, Union
 
 from agentforge_contracts.core.models.base_models import BaseModel, BaseResponse
-from agentforge_contracts.core.models.model_models import ModelCapability, ModelConfig, ModelType
+from agentforge_contracts.core.models.model_models import (
+    ModelCapability,
+    ModelConfig,
+    ModelType,
+)
 
 
 class ModelRequest(BaseModel):
